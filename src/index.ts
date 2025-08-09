038a5c7048ac20d6b531d5da8b9d62f1e4ba5af1
dxxmed <123851757+dxxmed@users.noreply.github.com>
2025-08-09 22:00:32 +0000

    minor tweaks

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import http from "http";
 const PORT = 5000;
 
 const Server = http.createServer((req, res) => {
-    if (req.url === "/" || req.url === "/home") {
+    if (req.url === "/" || req.url.match("/home")) {
         res.setHeader("Content-Type", "application/json");
         res.end(JSON.stringify({
             message: "Home Page"
         }));
-    } else if (req.url === "/users") {
+    } else if (req.url.match("/users")) {
         res.setHeader("Content-Type", "application/json");
         res.end(JSON.stringify({
             message: "Users Page"
