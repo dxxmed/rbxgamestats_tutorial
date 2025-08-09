c5c6c41db50dbee36454805c7b58749a9003c753
dxxmed <123851757+dxxmed@users.noreply.github.com>
2025-08-09 21:32:54 +0000

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,19 @@
 import http from "http";
-import Middleware from "./middleware";
 
 const PORT = 5000;
 
 const Server = http.createServer((req, res) => {
-    Middleware(req, res, () => {
-        if (req.url === "/" || req.url === "/home") {
-            res.setHeader("Content-Type", "application/json");
-            res.end({
-                message: "Home Page"
-            });
-            return;
-        } else if (req.url === "/users") {
-            res.setHeader("Content-Type", "application/json");
-            res.end({
-                message: "We'll add a Users Page here soon!"
-            });
-            return;
-        }
-    });
+    if (req.url === "/" || req.url === "/home") {
+        res.setHeader("Content-Type", "application/json");
+        res.end({
+            message: "Home Page"
+        });
+    } else if (req.url === "/users") {
+        res.setHeader("Content-Type", "application/json");
+        res.end({
+            message: "Users Page"
+        });
+    }
 });
 
 Server.listen(PORT, () => {
--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-import http from "http";
-
-export default function Middleware(req: http.IncomingMessage, res: http.ServerResponse, next: () => void) {
-    console.log(`Endpoint: ${req.url}`)
-    next();
-};
