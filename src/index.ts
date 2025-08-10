1c76908339e8f649f23b94dc323b88fbfdbdf866
dxxmed <123851757+dxxmed@users.noreply.github.com>
2025-08-10 23:30:07 +0000

    dynamic files update and more

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import http from "http";
+import fs from "fs";
+import url from "url";
 import mongoose from "mongoose";
+import mimetypes from "mime-types";
 import { config } from "dotenv";
 import Product, { Schema } from "./Models/model.js"
 
@@ -28,12 +31,85 @@ function GetBody(req: http.IncomingMessage): Promise<object> {
     });
 };
 
+async function ServeFile(req: http.IncomingMessage, res: http.ServerResponse) {
+    const URL = url.parse(req.url, true);
+    let FileName = URL.path.replace(/^\/+|\/+$/g, "");
+
+    if (FileName === "") FileName = "index.html";
+
+    const FilePath = import.meta.dirname.replace("/dist", "") + "/public/" + FileName;
+    const ClonedFilePath = FilePath.replace(FileName, "dynamicindex.html");
+    console.log(ClonedFilePath);
+    if (FileName === "index.html") {
+        const Data = await Product.find({});
+        
+        fs.copyFileSync(FilePath, ClonedFilePath);
+
+        Data.forEach(rawUser => {
+            let User = rawUser.toJSON();
+            let DynamicContent = [
+                "<div class=\"DynamicUsers\">",
+                `<h1>UserId: ${User.UserId.toString()}</h1>`,
+                "<h2>Items</h2>",
+                "<ul>",
+            ];
+            Object.keys(User.Items).forEach(key => {
+                DynamicContent.push(`<li>${key} - Name: ${User.Items[key]?.ItemName} & Value: ${User.Items[key]?.ItemValue}</li>`);
+            });
+
+            DynamicContent.push("</ul>");
+            DynamicContent.push("<h2>Moderation History</h2>");
+
+            Object.keys(User.ModerationHistory).forEach(key => {
+                DynamicContent.push(`Iden#: ${User.ModerationHistory[key]?.IdentifyingNumber}, Context: ${User.ModerationHistory[key]?.Context}`);
+            });
+            DynamicContent.push("</div>");
+        
+            try {
+                const FileData = fs.readFileSync(ClonedFilePath, "utf-8");
+        
+                if (!FileData) {
+                    res.writeHead(404, {"Content-Type": "application/json"});
+                    res.end(JSON.stringify({
+                        message: "File not found!",
+                    }));
+                    return;
+                };
+
+                const Result = FileData.replace(/\<\/body>/g, DynamicContent.join("") + "</body>");
+                fs.writeFileSync(ClonedFilePath, Result, "utf-8");
+            } catch(err) {
+                res.writeHead(500, {"Content-Type": "application/json"});
+                res.end(JSON.stringify({
+                    message: "There was a problem writing to index.html!",
+                }));
+                return;
+            };
+        });
+    };
+    fs.readFile(FileName === "index.html" ? ClonedFilePath : FilePath, (err, FileData) => {
+        if (err) {
+            res.writeHead(404, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({
+                message: "File not found!",
+            }));
+            return;
+        }
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        const FileMime = mimetypes.lookup(FileName) as string;
+        res.writeHead(200, {"Content-Type": FileMime});
+        res.end(FileData);
+        if (fs.existsSync(ClonedFilePath)) {
+            fs.unlinkSync(ClonedFilePath)
+        };
+    });
+};
+
 const Server = http.createServer(async (req, res) => {
-    if (req.url === "/" || req.url.match("/home") && req.method === "GET") {
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify({
-            message: "Home Page"
-        }));
+    console.log(`${req.method} : ${req.url}`);
+    if (req.url.match("/") || req.url.match("/home") && req.method === "GET") {
+        ServeFile(req, res);
+        return;
     } else if (req.url.match("/users") && req.method === "GET") {
         if (req.url.match(/\/users\/([0-9]+)/)) {
             const UserId = parseInt(req.url.split("/")[2]);
